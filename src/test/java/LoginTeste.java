8ff4ede097b861d4283208985b0798c28db5fc43
Calazhen <henriquecalazans.96@outlook.com>
2023-05-29 21:40:54 -0300

    ""

--- src/test/java/LoginTeste.java
+++ src/test/java/LoginTeste.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertThat;
 public class LoginTeste {
     final String driverPath = "C:\\Users\\calaz\\geckodriver.exe";
     private WebDriver driver;
+    //
 
 
     @BeforeEach
